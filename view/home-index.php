32c7e90fa9bcd1f4ad2c889d6eef24d8375b2389
melisandreschofield <melisandre.schofield@gmail.com>
2025-01-15 16:13:45 -0500

    reworking the "home-container" the content on the top of the home page.

--- view/home-index.php
+++ view/home-index.php
@@ -4,14 +4,28 @@
     <div class="home-container">
         <div class="background-svg"></div>
         <div class="text-container">
-            <p>{{message}}</p>
+           <!--  <p>{{message}}</p> -->
             <h1>collaborative writing</h1>
             <p>Add a few words to someone else's.</p>
             <p>Write a sentence, and see it grow legs.</p>
             <p>Write a phrase, and watch its head roll away.</p>
         </div>
+        <div class="buttons-container">
+            <a class="home-button login" href="{{path}}/login">
+                <span class="svg-btn" data-svg="logIn"></span>
+                <span>login</span>
+            </a>
+            <a class="home-button browse" href="{{path}}text">
+                <span class="svg-btn" data-svg="browse"></span>
+                <span>browse collaborations</span>
+            </a>
+            <a class="home-button how" href="#how">
+                <span class="svg-btn" data-svg="how"></span>
+                <span>how it works</span>
+            </a>
+        </div>
     </div>
-    <div class="tutorial">
+    <div class="tutorial" id="how">
         <h2>join a collaboration</h2>
         <ul>
             <li>create an account and/or log in</li>
